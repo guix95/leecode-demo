1e375a91505146e14642d50ef2b24f213559271c
guix95 <guix95@163.com>
2021-03-30 09:25:37 +0800

    创建leecode 刷题项目 , 上传 66题

---
+++ src/main/java/com/guix/leecodedemo/LeecodeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.guix.leecodedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeecodeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeecodeDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/guix/leecodedemo/LeecodeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guix.leecodedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeecodeDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
